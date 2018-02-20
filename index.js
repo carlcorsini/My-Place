b7443b110dd372031e872398d9a1334a9e8156c2
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-02-20 00:25:26 -0800

    Site in its final stages, just minor tweaks from here. Got all input boxes working. Image url. More styling

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   let form = document.querySelector(".website-form")
-  let primaryThemeOptions = document.querySelector(".primary-theme-options")
-  let ambienceOptions = document.querySelector(".ambience-options")
-  let fontOptions = document.querySelector(".font-options")
-  let myContainer = document.getElementsByClassName('my-container')
-  form.addEventListener('submit', changes);
   let exampleButton = document.querySelector('.example-button')
+  form.addEventListener('submit', changes);
   exampleButton.addEventListener('click', example)
 })
 
@@ -14,37 +10,59 @@ function example (event) {
   let bodyOptions = document.querySelector(".body-options")
   let headerOptions = document.querySelector(".header-options")
   let primaryThemeOptions = document.querySelector(".primary-theme-options")
+  let secondaryThemeOptions = document.querySelector(".secondary-theme-options")
   let bodyValue = bodyOptions.value
-  let themeValue = primaryThemeOptions.value
+  let primaryValue = primaryThemeOptions.value
+  let secondaryValue = secondaryThemeOptions.value
   let headerValue = headerOptions.value
   let fontExample = document.querySelector('.font-example')
-  let fontDiv = document.querySelector('.font-example-div')
+  let primaryDiv = document.querySelector('.font-example-div')
   let headerExample = document.querySelector('.header-example')
-  fontDiv.id = themeValue
+  let secondaryDiv = document.querySelector('.secondary-div')
+  primaryDiv.id = primaryValue
+  secondaryDiv.id = secondaryValue
   fontExample.id = bodyValue
   headerExample.id = headerValue
 }
 
 
 function changes (event) {
-  // event.preventDefault()
-  let myContainer = document.getElementsByClassName('my-container')
+  // options
   let primaryThemeOptions = document.querySelector(".primary-theme-options")
   let secondaryThemeOptions = document.querySelector(".secondary-theme-options")
   let ambienceOptions = document.querySelector(".ambience-options")
   let bodyOptions = document.querySelector(".body-options")
   let headerOptions = document.querySelector('.header-options')
+  // text input
+  let headerInput = document.querySelector('.header-input')
+  let footerInput = document.querySelector('.footer-input')
+  let img1Input = document.querySelector('.image1-input')
+  let img2Input = document.querySelector('.image2-input')
+  let img3Input = document.querySelector('.image3-input')
+  let bio1Input = document.querySelector('.bio1-input')
+  let bio2Input = document.querySelector('.bio2-input')
+  let bio3Input = document.querySelector('.bio3-input')
+
     //theme options
       localStorage.setItem('primaryThemeOptions', JSON.stringify(primaryThemeOptions.value))
       localStorage.setItem('secondaryThemeOptions', JSON.stringify(secondaryThemeOptions.value))
-
     //font options
       localStorage.setItem('bodyOptions', JSON.stringify(bodyOptions.value))
       localStorage.setItem('headerOptions', JSON.stringify(headerOptions.value))
     //ambience options
+      localStorage.setItem('ambienceOptions', JSON.stringify(ambienceOptions.value))
+    //header & footer input
+      localStorage.setItem('headerInput', JSON.stringify(headerInput.value))
+      localStorage.setItem('footerInput', JSON.stringify(footerInput.value))
+    //image input
+      localStorage.setItem('image1Input', JSON.stringify(img1Input.value))
+      localStorage.setItem('image2Input', JSON.stringify(img2Input.value))
+      localStorage.setItem('image3Input', JSON.stringify(img3Input.value))
 
-
-
+    //bio input
+      localStorage.setItem('bio1Input', JSON.stringify(bio1Input.value))
+      localStorage.setItem('bio2Input', JSON.stringify(bio2Input.value))
+      localStorage.setItem('bio3Input', JSON.stringify(bio3Input.value))
 }
 
   // const favorites = JSON.parse(localStorage.getItem('favorites'))||{};
--- index2.js
+++ index2.js
@@ -1,17 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
   let primaryThemeStorage = JSON.parse(localStorage.getItem('primaryThemeOptions'));
   let secondaryThemeStorage = JSON.parse(localStorage.getItem('secondaryThemeOptions'));
-  let ambienceStorage = JSON.parse(localStorage.getItem('primaryThemeOptions'));
   let headerStorage = JSON.parse(localStorage.getItem('headerOptions'));
   let bodyStorage = JSON.parse(localStorage.getItem('bodyOptions'));
+  let ambienceStorage = JSON.parse(localStorage.getItem('ambienceOptions'));
+  let headerOutput = JSON.parse(localStorage.getItem('headerInput'));
+  let footerOutput = JSON.parse(localStorage.getItem('footerInput'));
+  let image1Output = JSON.parse(localStorage.getItem('image1Input'))
+  let image2Output = JSON.parse(localStorage.getItem('image2Input'))
+  let image3Output = JSON.parse(localStorage.getItem('image3Input'))
+  let bio1Output = JSON.parse(localStorage.getItem('bio1Input'))
+  let bio2Output = JSON.parse(localStorage.getItem('bio2Input'))
+  let bio3Output = JSON.parse(localStorage.getItem('bio3Input'))
   let row1 = document.querySelector('.row1')
   let row2 = document.querySelector('.row2')
   let rowRight = document.querySelector('.row-right')
   let myContainer = document.querySelector('.my-container');
   let header = document.querySelector('.header')
+  let footer = document.querySelector('.footer')
+  let audio = document.querySelector('.audio-source')
+  let img1 = document.querySelector('.img1')
+  let img2 = document.querySelector('.img2')
+  let img3 = document.querySelector('.img3')
+  let bio1 = document.querySelector('.bio1')
+  let bio2 = document.querySelector('.bio2')
+  let bio3 = document.querySelector('.bio3')
       myContainer.classList.add(primaryThemeStorage)
       row1.classList.add(bodyStorage, secondaryThemeStorage)
       row2.classList.add(bodyStorage, secondaryThemeStorage)
       rowRight.classList.add(bodyStorage, secondaryThemeStorage)
-      header.classList.add(headerStorage)
+      header.classList.add(headerStorage, secondaryThemeStorage)
+      footer.classList.add(headerStorage, secondaryThemeStorage)
+      header.innerHTML = headerOutput
+      footer.innerHTML = footerOutput
+      audio.src = ambienceStorage
+      img1.src = image1Output || "urdad.jpg"
+      img2.src = image2Output || "urdad.jpg"
+      img3.src = image3Output || "urdad.jpg"
+      bio1.innerHTML = bio1Output
+      bio2.innerHTML = bio2Output
+      bio3.innerHTML = bio3Output
 })
